d91ee65b10abc38b695d2caf4713af4bd965f3a9
umardy <41959863+umardy@users.noreply.github.com>
2021-10-08 18:08:41 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(long_description)
 
 setup(
     name='chromedriver-autodownloader',
-    version='0.1.0',
+    version='0.2.0',
     license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
     author="Umardy",
     author_email='umardev004@gmail.com',
@@ -15,13 +15,13 @@ setup(
         "License :: OSI Approved :: Apache Software License", 
         "Programming Language :: Python :: 3", 
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         ], 
     packages=['chromedriver_autodownloader'],
     url='https://github.com/umardy/chromedriver-autodownloader',
     keywords='automatic chromedriver downloader',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=[
-          'requests',
-      ],
 )
